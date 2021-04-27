8d139cf7bd41a2b466f73de5f24172fb29e29db9
Sumitra Malagi <46084757+sumitramalagi@users.noreply.github.com>
2021-04-27 12:59:44 +0530

    Update download_snli.sh
    changed unzip to sudo

--- download_snli.sh
+++ download_snli.sh
@@ -3,7 +3,7 @@ mkdir ./data/snli
 
 wget https://nlp.stanford.edu/projects/snli/snli_1.0.zip -O ./data/snli/data.zip
 
-unzip ./data/snli/data.zip 
+sudo unzip data/snli/data -d data/snli
 mv ./snli_1.0/* ./data/snli
 rm -r ./snli_1.0/
 rm data/snli/data.zip
